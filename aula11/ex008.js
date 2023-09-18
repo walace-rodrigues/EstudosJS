41d2945a91121d2c36057fd714bca182648442a3
Walace Rodrigues <walaceclash10@gmail.com>
2023-09-18 14:47:14 -0300

    aulas

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é de ${vel}KM por hora`)
+if (vel > 50){
+    console.log(`Você foi multado por excesso de velocidade!`)
+}
+else {
+    console.log(`Não se esqueça do sinto de segurança`)
+}
